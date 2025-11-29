4818b9cb2c3cb62b40c512d5c6ee0c85792b0356
Fabian Frank <fabian@brmlabs.co>
2025-11-29 13:01:17 -0800

    eslint: remove config compatibility shim
    our config is pretty simple, we can migrate it

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,51 +1,48 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-import promisePlugin from "eslint-plugin-promise";
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import promisePlugin from 'eslint-plugin-promise';
+import globals from 'globals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([globalIgnores(["**/dist"]), {
-    extends: compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ),
-
-    plugins: {
-        promise: promisePlugin,
-    },
-
+export default [
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['src/**/*.ts'],
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2018,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        NodeJS: 'readonly',
+      },
     },
-
+    plugins: {
+      '@typescript-eslint': tseslint,
+      promise: promisePlugin,
+    },
     rules: {
-        "dot-notation": "off",
-        eqeqeq: "warn",
-        curly: ["warn", "all"],
-        "prefer-arrow-callback": ["warn"],
-        "no-console": ["warn"],
-        "no-non-null-assertion": ["off"],
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
 
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
+      'dot-notation': 'off',
+      eqeqeq: 'warn',
+      curly: ['warn', 'all'],
+      'prefer-arrow-callback': ['warn'],
+      'no-console': ['warn'],
+      'no-non-null-assertion': ['off'],
 
-        // Enforce async/await over .then()/.catch()
-        "promise/prefer-await-to-then": "error",
-        "promise/prefer-await-to-callbacks": "warn",
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      // Enforce async/await over .then()/.catch()
+      'promise/prefer-await-to-then': 'error',
+      'promise/prefer-await-to-callbacks': 'warn',
     },
-}]);
+  },
+  prettier,
+];
